eb62bc562322bdbf328d58a73981c0605c13dec8
chivenice <chivenice29@gmail.com>
2021-09-30 23:56:30 +0900

    improve code

--- Mouse Movement/app.js
+++ Mouse Movement/app.js
@@ -21,58 +21,72 @@ var input = {
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
 input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
+//output setup
+var output = {
+  x: {
+    start: -150,
+    end: 150,
+    current: 0,
+  },
+  y: {
+    start: -150,
+    end: 150,
+    current: 0,
+  },
+  zIndex:{
+    range:10000
+  }
+};
+output.x.range = output.x.end - output.x.start;
+output.y.range = output.y.end - output.y.start;
 
-var handleMouseMove = function (event) {
-  // mouse x input
-  input.mouseX.current = event.pageX;
+var mouse = {
+  x: 0,
+  y: 0
+}
+
+var updateInputs = function() {
+  // mouse x input and y input
+  input.mouseX.current = mouse.x;
   input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
 
-  // mouse y input
-  input.mouseY.current = event.pageY;
+  input.mouseY.current = mouse.y;
   input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
+}
 
-
-  //output setup
-  var output = {
-    x: {
-      start: -150,
-      end: 150,
-      current: 0,
-    },
-    y: {
-      start: -150,
-      end: 150,
-      current: 0,
-    },
-  };
-  output.x.range = output.x.end - output.x.start;
-  output.y.range = output.y.end - output.y.start;
-
-  //output x
+var updateOutputs = function() { 
+  //output x and y
   output.x.current = output.x.end - (input.mouseX.fraction * output.x.range);
-
-  //output　y
   output.y.current = output.y.end - (input.mouseY.fraction * output.y.range);
+}
+
+// De dang thay doi thong so va thiet ke
+var handleMouseMove = function (event) {
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+  updateInputs();
+  updateOutputs();
 
   //apply output to html
   itemsArray.forEach(function (item, k) {
-    var depth = parseFloat(item.dataset.depth,10);    //10 convert a number or a piece of string to something that count by 15s; 
+    var depth = parseFloat(item.dataset.depth, 10);    //10 convert a number or a piece of string to something that count by 15s; 
     var itemOutput = {
-     x: output.x.current - (output.x.current * depth),
-     y: output.y.current -(output.y.current * depth),
-     zIndex:10000 - (10000*depth)
-   };
-        console.log(k,'depth',depth)
-     item.style.zIndex= itemOutput.zIndex;
-     item.style.transform = 'translate(' + itemOutput.x + 'px, ' + itemOutput.y + 'px)';
- 
+      x: output.x.current - (output.x.current * depth),
+      y: output.y.current - (output.y.current * depth),
+      zIndex: output.zIndex.range - ( output.zIndex.range*depth)
+    };
+    console.log(k, 'depth', depth)
+    item.style.zIndex = itemOutput.zIndex;
+    item.style.transform = 'translate(' + itemOutput.x + 'px, ' + itemOutput.y + 'px)';
 
- });
 
-  //console.log('output.x.current',output.x.current);
-  //console.log('fraction Y',input.mouseY.fraction);
+  });
+}
 
-  }
+//console.log('output.x.current',output.x.current);
+//console.log('fraction Y',input.mouseY.fraction);
+
+
 
 var handleResize = function () {
   input.mouseX.end = window.innerWidth;

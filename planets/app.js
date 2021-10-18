6c52926b4e8d5860381be719d425134d9a8e6da4
chivenice <chivenice29@gmail.com>
2021-10-19 00:32:25 +0900

    blur added

--- planets/app.js
+++ planets/app.js
@@ -1,4 +1,13 @@
 var parallaxContainer = document.getElementById('parallaxContainer');
+
+//setup output
+var output = {
+    blur:{
+        start:0.2,
+        range:20,
+    }
+}
+
 for (var i = 0; i < 10; i++) {
     //create a new element with parallax-container className
     var item = document.createElement('div');
@@ -16,14 +25,17 @@ for (var i = 0; i < 10; i++) {
     var bgImgNum = Math.round(randomNum * 10);
     var rotateNum = (360 * randomNum);
     var depth =randomNum;
+    var blur =(depth-output.blur.start)*output.blur.range;
 
-    item.style.width = 500 * randomNum + 200;
-    item.style.height = 500 * randomNum + 200;
+
+    item.style.filter = 'blur('+blur+'px)';
+    item.style.width = 500 * randomNum + 50;
+    item.style.height = 500 * randomNum + 50;
     item.dataset.depth = randomNum;
     item.style.transform = 'rotate(' + (360 * randomNum) + 'deg)';
     planet.style.backgroundImage = 'url(/Users/chivenice/Desktop/Personal/dumb-art/planet'+ bgImgNum +'.png)';
+    item.style.top = Math.round(randomNum * window.innerHeight-50) + 'px';
+    item.style.left = Math.round(randomNum * window.innerWidth-100) + 'px';
 
-    //adjust blur on depth
-    var blur =(depth-output.blur.start)*output.blur.range;
-    item.style.filter = 'blur(20px)';
 }
+    //adjust blur on depth
